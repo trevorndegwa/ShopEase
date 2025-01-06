5c6d2d19a68407de4e65d3c43f185c368dbd3a63
Trevor Ndegwa <tntrende@gmail.com>
2025-01-06 18:22:26 +0300

    tested out administrator section

--- ShopEase/store/models.py
+++ ShopEase/store/models.py
@@ -8,6 +8,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
 # Customer model
 class Customer(models.Model):
     first_name = models.CharField(max_length=75)
