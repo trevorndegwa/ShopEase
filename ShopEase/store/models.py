b8168bbab4eaf3a8e161fad3132e85ec25a41498
Trevor Ndegwa <tntrende@gmail.com>
2025-01-07 11:54:35 +0300

    Modified models to reflect sale effect, made migrations, and migrated

--- ShopEase/store/models.py
+++ ShopEase/store/models.py
@@ -29,6 +29,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
+    # Add some sale effects
+    is_on_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=8)
 
     def __str__(self):
         return self.name  
