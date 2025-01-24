c222142706add8a33008774fbc8c545f706ef259
Trevor Ndegwa <tntrende@gmail.com>
2025-01-24 20:14:34 +0300

    Fixed indetation issues in cart.py

--- ShopEase/cart/cart.py
+++ ShopEase/cart/cart.py
@@ -12,14 +12,14 @@ class Cart():
         # Ensure the cart is available on all site pages
         self.cart = cart
 
-def add(self, product):
-    # Convert the product ID to a string to use it as a key in the cart dictionary
-    product_id = str(product.id)
-    
-    # Control logic checking if product exists within the cart
-    if product_id in self.cart:
-        pass
-    else:
-        self.cart[product_id] = {'price': str(product.price)}
-    # Mark the session as modified to ensure changes to the cart are saved
-    self.session.modified = True
+    def add(self, product):
+        # Convert the product ID to a string to use it as a key in the cart dictionary
+        product_id = str(product.id)
+            
+        # Control logic checking if product exists within the cart
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+        # Mark the session as modified to ensure changes to the cart are saved
+        self.session.modified = True
