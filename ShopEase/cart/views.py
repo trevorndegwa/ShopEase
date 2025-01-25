04d8d3ef74bed35840437aaf106a53660f1ee050
Trevor Ndegwa <tntrende@gmail.com>
2025-01-25 10:52:09 +0300

    Resized drop down menu, cart quantity ID, and added the reference to the views.py in cart

--- ShopEase/cart/views.py
+++ ShopEase/cart/views.py
@@ -10,10 +10,11 @@ def cart_add(request):
     if request.POST.get('action') == 'post':
         # Get the sent data
         product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
         # Find the product in the database
         product = get_object_or_404(Product, id=product_id)
         # Save to the session
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_quantity)
         # Get number of items in cart
         cart_number = cart.__len__()
         # Return the response
