163366f37eb4e8187d2f66ad767ab31b1cdc716f
Trevor Ndegwa <tntrende@gmail.com>
2025-01-26 19:55:17 +0300

    Edited shipping model and made migrations

--- ShopEase/payment/models.py
+++ ShopEase/payment/models.py
@@ -3,20 +3,20 @@ from django.contrib.auth.models import User
 
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    names = models.CharField(max_length=250)
-    email = models.EmailField(max_length=250)
-    address1 = models.CharField(max_length=250)
-    address2 = models.CharField(max_length=250, null=True, blank=True)
-    country = models.CharField(max_length=250, default="Kenya")
-    county = models.CharField(max_length=250, null=True, blank=True)
-    city = models.CharField(max_length=250)
-    postalcode = models.CharField(max_length=250, null=True, blank=True)
+    fullname_shipping = models.CharField(max_length=250)
+    email_shipping = models.EmailField(max_length=250)
+    address1_shipping = models.CharField(max_length=250)
+    address2_shipping = models.CharField(max_length=250, null=True, blank=True)
+    country_shipping = models.CharField(max_length=250, default="Kenya")
+    county_shipping = models.CharField(max_length=250, null=True, blank=True)
+    city_shipping = models.CharField(max_length=250)
+    postalcode_shipping = models.CharField(max_length=250, null=True, blank=True)
 
     # Admin backend don't pluralise "Shipping Address"
     class Meta:
         verbose_name_plural = "Shipping Address"
-        ordering = ['names']  # Order by names
+        ordering = ['fullname_shipping']  # Order by names
 
     def __str__(self):
-        return f'{self.names} - {self.address1}, {self.city}'
+        return f'{self.fullname_shipping} - {self.address1_shipping}, {self.city_shipping}'
 
