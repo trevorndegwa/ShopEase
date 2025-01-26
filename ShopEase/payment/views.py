5282163cdb08ed15220102e7074c060a1a866f39
Trevor Ndegwa <tntrende@gmail.com>
2025-01-26 22:23:20 +0300

    Defined checkout view to get order summary data

--- ShopEase/payment/views.py
+++ ShopEase/payment/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from cart.cart import Cart
 
 def payment_successful(request):
     return render(request, "payment/payment_successful.html", {})
+
+def checkout(request):
+    # Acquire the cart
+    cart = Cart(request)
+    cart_products = cart.get_products
+    quantities = cart.get_quantities
+    sums = cart.cart_total()
+    return render(request, "payment/checkout.html", {
+        "cart_products": cart_products, 
+	"quantities": quantities, 
+	"sums": sums
+    })
