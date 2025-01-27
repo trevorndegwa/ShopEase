6e6fda44c0e6af84d0c12876fdb8a26a61d34a4f
Trevor Ndegwa <tntrende@gmail.com>
2025-01-27 03:31:53 +0300

    Altered admin.py for order and order items

--- ShopEase/payment/admin.py
+++ ShopEase/payment/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import ShippingAddress, Order, OrderItem
 
 # On the admin site, register the models
 admin.site.register(ShippingAddress)
 admin.site.register(OrderItem)
 admin.site.register(Order)
+
+# Creating OrderItem Inline
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+# Extend the Order Model
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = [OrderItemInline]
+
+# Unregister the Order Model
+admin.site.unregister(Order)
+# Re-Register the Order && OrderAdmin
+admin.site.register(Order, OrderAdmin)
